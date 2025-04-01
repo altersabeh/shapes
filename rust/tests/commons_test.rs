9b5e4181bf698264a7bd9c481b3b2a017a8c9819
AlterSabeh <betamaximum22@gmail.com>
2025-04-01 18:45:37 +0800

    refactor tests to use 'Dimensional' terminology and update assertion functions

--- rust/tests/commons_test.rs
+++ rust/tests/commons_test.rs
@@ -1,35 +1,35 @@
 mod fixtures;
 
-mod measurable_test {
+mod dimensional_test {
     use shapes::base::commons::Dimensional;
 
     use super::*;
 
     #[test]
-    fn test_circle_implements_measurable() {
+    fn test_circle_implements_dimensional() {
         let circle = fixtures::setup_circle();
-        assert_measurable(circle, 754.767, 97.389);
+        assert_dimensional(circle, 754.767, 97.389);
     }
 
     #[test]
-    fn test_rectangle_implements_measurable() {
+    fn test_rectangle_implements_dimensional() {
         let rectangle = fixtures::setup_rectangle();
-        assert_measurable(rectangle, 756.437, 112.0);
+        assert_dimensional(rectangle, 756.437, 112.0);
     }
 
     #[test]
-    fn test_square_implements_measurable() {
+    fn test_square_implements_dimensional() {
         let square = fixtures::setup_square();
-        assert_measurable(square, 105.062, 41.0);
+        assert_dimensional(square, 105.062, 41.0);
     }
 
-    fn assert_measurable(
-        geometric_shape: impl Dimensional,
+    fn assert_dimensional(
+        dimensional_shape: impl Dimensional,
         expected_area: f64,
         expected_perimeter: f64,
     ) {
-        let area_diff = (geometric_shape.area() - expected_area).abs();
-        let perimeter_diff = (geometric_shape.perimeter() - expected_perimeter).abs();
+        let area_diff = (dimensional_shape.area() - expected_area).abs();
+        let perimeter_diff = (dimensional_shape.perimeter() - expected_perimeter).abs();
 
         assert!(area_diff < 0.001, "AREA NOT EQUAL");
         assert!(perimeter_diff < 0.001, "PERIMETER NOT EQUAL!");
