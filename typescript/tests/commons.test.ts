b130108e3c34633a41fb3bf574c78a6fba6a29bf
AlterSabeh <betamaximum22@gmail.com>
2025-04-01 18:46:25 +0800

    refactor tests to use 'Dimensional' terminology and update assertion functions

--- typescript/tests/commons.test.ts
+++ typescript/tests/commons.test.ts
@@ -2,30 +2,30 @@ import { Dimensional } from "shapes/base/commons";
 import { describe, expect, it } from "vitest";
 import * as fixtures from "./fixtures";
 
-describe("Measurable Tests", () => {
-    it("Test Circle Implements Measurable", () => {
+describe("Dimensional Tests", () => {
+    it("Test Circle Implements Dimensional", () => {
         const circle = fixtures.setupCircle();
-        assertMeasurable(circle, 754.767, 97.389);
+        assertDimensional(circle, 754.767, 97.389);
     });
 
-    it("Test Rectangle Implements Measurable", () => {
+    it("Test Rectangle Implements Dimensional", () => {
         const rectangle = fixtures.setupRectangle();
-        assertMeasurable(rectangle, 756.437, 112.0);
+        assertDimensional(rectangle, 756.437, 112.0);
     });
 
-    it("Test Square Implements Measurable", () => {
+    it("Test Square Implements Dimensional", () => {
         const square = fixtures.setupSquare();
-        assertMeasurable(square, 105.062, 41.0);
+        assertDimensional(square, 105.062, 41.0);
     });
 });
 
-export function assertMeasurable(
-    geometricShape: Dimensional,
+export function assertDimensional(
+    dimensionalShape: Dimensional,
     expectedArea: number,
     expectedPerimeter: number
 ) {
-    const areaDiff = Math.abs(geometricShape.area() - expectedArea);
-    const perimeterDiff = Math.abs(geometricShape.perimeter() - expectedPerimeter);
+    const areaDiff = Math.abs(dimensionalShape.area() - expectedArea);
+    const perimeterDiff = Math.abs(dimensionalShape.perimeter() - expectedPerimeter);
 
     expect(areaDiff, "AREA NOT EQUAL").toBeLessThan(0.001);
     expect(perimeterDiff, "PERIMETER NOT EQUAL!").toBeLessThan(0.001);
