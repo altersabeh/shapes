5884ce67da0a789af7a2ad895cd84449da12d70e
AlterSabeh <betamaximum22@gmail.com>
2025-04-01 18:44:49 +0800

    refactor tests to use 'Dimensional' terminology and update assertion functions

--- python/tests/test_commons.py
+++ python/tests/test_commons.py
@@ -3,27 +3,27 @@ import fixtures
 from shapes.base.commons import Dimensional
 
 
-class TestMeasurable:
-    def test_circle_implements_measurable(self):
+class TestDimensional:
+    def test_circle_implements_dimensional(self):
         circle = fixtures.setup_circle()
-        assert_measurable(circle, 754.767, 97.389)
+        assert_dimensional(circle, 754.767, 97.389)
 
-    def test_rectangle_implements_measurable(self):
+    def test_rectangle_implements_dimensional(self):
         rectangle = fixtures.setup_rectangle()
-        assert_measurable(rectangle, 756.437, 112.0)
+        assert_dimensional(rectangle, 756.437, 112.0)
 
-    def test_square_implements_measurable(self):
+    def test_square_implements_dimensional(self):
         square = fixtures.setup_square()
-        assert_measurable(square, 105.062, 41.0)
+        assert_dimensional(square, 105.062, 41.0)
 
 
-def assert_measurable(
-    geometric_shape: Dimensional,
+def assert_dimensional(
+    dimensional_shape: Dimensional,
     expexted_area: float,
     expected_perimeter: float,
 ):
-    area_diff = abs(geometric_shape.area() - expexted_area)
-    perimeter_diff = abs(geometric_shape.perimeter() - expected_perimeter)
+    area_diff = abs(dimensional_shape.area() - expexted_area)
+    perimeter_diff = abs(dimensional_shape.perimeter() - expected_perimeter)
 
     assert area_diff < 0.001, "AREA NOT EQUAL"
     assert perimeter_diff < 0.001, "PERIMETER NOT EQUAL!"
