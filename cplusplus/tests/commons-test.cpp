28a765a46003eca4f7b1f1bb4d6ecf504483dff2
AlterSabeh <betamaximum22@gmail.com>
2025-04-01 18:49:13 +0800

    refactor tests to use 'Dimensional' terminology in assertions and test cases

--- cplusplus/tests/commons-test.cpp
+++ cplusplus/tests/commons-test.cpp
@@ -6,31 +6,31 @@ using namespace std;
 
 namespace shapes::tests {
 namespace {
-    void assertMeasurable(
-        const Dimensional& geometricShape,
+    void assertDimensional(
+        const Dimensional& dimensionalShape,
         double expectedArea,
         double expectedPerimeter
     ) {
-        const double areaDiff = abs(geometricShape.area() - expectedArea);
-        const double perimeterDiff = abs(geometricShape.perimeter() - expectedPerimeter);
+        const double areaDiff = abs(dimensionalShape.area() - expectedArea);
+        const double perimeterDiff = abs(dimensionalShape.perimeter() - expectedPerimeter);
 
         EXPECT_TRUE(areaDiff < 0.001) << "AREA NOT EQUAL";
         EXPECT_TRUE(perimeterDiff < 0.001) << "PERIMETER NOT EQUAL!";
     }
 
-    TEST(CommonsTest, CircleImplementsMeasurable) {
+    TEST(CommonsTest, CircleImplementsDimensional) {
         auto circle = fixtures::setupCircle();
-        assertMeasurable(circle, 754.767, 97.389);
+        assertDimensional(circle, 754.767, 97.389);
     }
 
-    TEST(CommonsTest, RectangleImplementsMeasurable) {
+    TEST(CommonsTest, RectangleImplementsDimensional) {
         auto rectangle = fixtures::setupRectangle();
-        assertMeasurable(rectangle, 756.437, 112.0);
+        assertDimensional(rectangle, 756.437, 112.0);
     }
 
-    TEST(CommonsTest, SquareImplementsMeasurable) {
+    TEST(CommonsTest, SquareImplementsDimensional) {
         auto square = fixtures::setupSquare();
-        assertMeasurable(square, 105.062, 41.0);
+        assertDimensional(square, 105.062, 41.0);
     }
 } // namespace
 } // namespace shapes::tests
