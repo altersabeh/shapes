f49ba06394c50775835c1a8e36fa9d2458c215d8
AlterSabeh <betamaximum22@gmail.com>
2025-04-03 12:41:58 +0800

    add main module for Shapes application

---
+++ haskell/app/Shapes/App/Main.hs
@@ -0,0 +1,15 @@
+module Main (main) where
+
+import Shapes.Base.Commons
+import Shapes.Base.Models
+import Shapes.Client.Shell
+
+main :: IO ()
+main = do
+    let program :: Runner = Runner{}
+
+    let circle :: Circle = newCircle 15.25
+    let rcircle = setColor circle "Red"
+    printDescription rcircle
+
+    run program
