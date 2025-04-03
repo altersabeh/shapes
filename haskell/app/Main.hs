fb22ee4de02642869f2a86933513395701f22c50
AlterSabeh <betamaximum22@gmail.com>
2025-04-03 12:42:06 +0800

    remove Main module and update cabal file for new executable path

--- haskell/app/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import Shapes.Base.Commons
-import Shapes.Base.Models
-
-main :: IO ()
-main = do
-    let circle = Circle 5 "RED"
-    putStrLn $ "Circle area: " ++ show (area circle)
