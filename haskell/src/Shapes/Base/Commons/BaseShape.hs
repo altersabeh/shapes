43436838a276e9fc1f600e04af19faae629ee7c8
AlterSabeh <betamaximum22@gmail.com>
2025-04-03 12:41:51 +0800

    implement BaseShape class

--- haskell/src/Shapes/Base/Commons/BaseShape.hs
+++ haskell/src/Shapes/Base/Commons/BaseShape.hs
@@ -1,3 +1,12 @@
 module Shapes.Base.Commons.BaseShape where
 
-class BaseShape
+class BaseShape a where
+    dimensions :: a -> [(String, Double)]
+
+    printDescription :: a -> IO ()
+
+    getDimensionAvailable :: a -> Int
+    getDimensionAvailable shape = length (dimensions shape)
+
+    isAShape :: a -> Bool
+    isAShape _ = True
--- haskell/src/Shapes/Base/Models/Circle.hs
+++ haskell/src/Shapes/Base/Models/Circle.hs
@@ -1,4 +1,4 @@
-module Shapes.Base.Models.Circle where
+module Shapes.Base.Models.Circle (Circle (..), newCircle, getColor, setColor) where
 
 import Shapes.Base.Commons
 
@@ -6,9 +6,34 @@ data Circle = Circle {radius :: Double, color :: String} deriving (Show, Eq)
 
 instance Dimensional Circle where
     area (Circle radius _) = pi * radius * radius
+
     perimeter (Circle radius _) = 2 * pi * radius
 
 instance Displayable Circle
 
+instance BaseShape Circle where
+    dimensions (Circle radius _) = [("radius", (max radius 1)), ("diameter", 2 * (max radius 1)), ("curvature", 1 / (max radius 1))]
+
+    printDescription circle = do
+        putStrLn ("Circle radius: " ++ show (radius circle))
+        putStrLn ("Circle dimensions: " ++ formatDimensions (dimensions circle))
+        putStrLn ("Circle color: " ++ color circle)
+        putStrLn ("Circle area: " ++ show (area circle))
+        putStrLn ("Circle perimeter: " ++ show (perimeter circle))
+        putStrLn ("Circle is a shape: " ++ show (isAShape circle))
+
+    getDimensionAvailable circle = length (dimensions circle)
+
 newCircle :: Double -> Circle
 newCircle radius = Circle (max radius 1) "None"
+
+getColor :: Circle -> String
+getColor (Circle _ color) = color
+
+setColor :: Circle -> String -> Circle
+setColor (Circle radius _) newColor = Circle radius newColor
+
+formatDimensions :: [(String, Double)] -> String
+formatDimensions dims =
+    let formatted = unwords [key ++ ": " ++ show value ++ " |" | (key, value) <- dims]
+     in if not (null formatted) then init (init formatted) else formatted
