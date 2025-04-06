8fc4f33e664cb78659ce9352d2d47e5209d9aa4c
AlterSabeh <betamaximum22@gmail.com>
2025-04-06 14:49:40 +0800

    refactor printThankYouMessage to use explicit function type and simplify invocation

--- haskell/src/Shapes/Client/Shell/Runner.hs
+++ haskell/src/Shapes/Client/Shell/Runner.hs
@@ -24,13 +24,13 @@ run Runner {} = do
     putStrLn "Welcome to the Area and Perimeter Calculator!"
     putStrLn (replicate 53 '=')
 
-    printThankYouMessage :: () -> IO ()
-    printThankYouMessage _ = do
+    let printThankYouMessage :: IO ()
+        printThankYouMessage = do
             putStrLn "Thank you for using the Shapes CLI!"
             putStrLn "P.S. Did you know? The secret code is 42!"
 
-    actions :: Map.Map String (IO ())
-    actions =
+    let actions :: Map.Map String (IO ())
+        actions =
             Map.fromList
                 [ ("circle", Handler.handleCircle),
                   ("c", Handler.handleCircle),
@@ -44,7 +44,7 @@ run Runner {} = do
         startingMessage
         shape <- readInput
         when (shape == "q") $ do
-            printThankYouMessage ()
+            printThankYouMessage
             exitSuccess
         let action =
                 Map.findWithDefault
