b02cfbf73ce6e581a4a9352359384e1fd4166063
AlterSabeh <betamaximum22@gmail.com>
2025-04-18 20:28:53 +0800

    update .credo.exs to define configuration for Credo checks

--- elixir/.credo.exs
+++ elixir/.credo.exs
@@ -1,1 +1,14 @@
-%{}
+%{
+  configs: [
+    %{
+      name: "default",
+      files: %{
+        included: ["lib/", "test/"],
+        excluded: []
+      },
+      checks: [
+        {Credo.Check.Readability.ModuleDoc, false}
+      ]
+    }
+  ]
+}
